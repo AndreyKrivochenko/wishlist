18f954644cefc26ed30d970b34aa4873f4c9ba1b
AndreyKrivochenko <krivochenko_andrey@mail.ru>
2021-09-07 15:04:59 +0700

    Added django-environ

--- wishlist/wishlist/settings.py
+++ wishlist/wishlist/settings.py
@@ -12,15 +12,21 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import environ
+import os
+
+env = environ.Env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)d6k8&d&ie2-!gp@(46yi1v#tesljkc2urr7(vw^+2y1f)&zb$'
+
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
